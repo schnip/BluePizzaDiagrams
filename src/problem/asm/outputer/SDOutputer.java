ef514325e4b97ffb17e6d7d0f284fc43b42fa8bf
NithinPerumal <peruman@rose-hulman.edu>
2016-01-13 13:05:13 -0500

    New outputer created - no code yet

---
+++ src/problem/asm/outputer/SDOutputer.java
@@ -0,0 +1,13 @@
+package problem.asm.outputer;
+
+import problem.asm.storage.MetaDataLibrary;
+
+public class SDOutputer implements IOutputData{
+
+	@Override
+	public void outputData(MetaDataLibrary m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
