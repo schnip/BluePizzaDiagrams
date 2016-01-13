f909d2428a78282098943d18e62cc1288d59d4dd
schnip <pingthegreenflag@gmail.com>
2016-01-12 23:13:57 -0500

    stuff

---
+++ src/problem/asm/storage/UseSentence.java
@@ -0,0 +1,15 @@
+package problem.asm.storage;
+
+public class UseSentence {
+	
+	private String name;
+	
+	public UseSentence(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
