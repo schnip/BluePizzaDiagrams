fede42047e17f77bb96f020b8475e828a46083f0
schnip <pingthegreenflag@gmail.com>
2016-01-14 00:05:12 -0500

    Did some stuff

---
+++ sd/test.sh
@@ -0,0 +1,2 @@
+java -jar sdedit-4.2-beta1.jar -o out.png -t png out.sd
+eog out.png
--- src/problem/asm/outputer/SDOutputer.java
+++ src/problem/asm/outputer/SDOutputer.java
@@ -1,13 +1,27 @@
 package problem.asm.outputer;
 
+import java.io.PrintWriter;
+
 import problem.asm.storage.MetaDataLibrary;
 
 public class SDOutputer implements IOutputData{
+	
+	private String filePath;
+	
+	public SDOutputer(String s) {
+		filePath = s;
+	}
 
 	@Override
 	public void outputData(MetaDataLibrary m) {
-		// TODO Auto-generated method stub
-		
+		try {
+			// Setup the header
+			PrintWriter writer = new PrintWriter(this.filePath, "UTF-8");
+			
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
