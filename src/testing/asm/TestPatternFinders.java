e8e546e94e478720e52648ec7289fd1d34286d66
schnip <pingthegreenflag@gmail.com>
2016-01-24 23:10:02 -0500

    Moar tests

--- src/testing/asm/TestPatternFinders.java
+++ src/testing/asm/TestPatternFinders.java
@@ -38,4 +38,43 @@ public class TestPatternFinders {
 		}
 	}
 	
+	@SuppressWarnings("resource")
+	@Test
+	public void testSingletonDesktop() throws IOException {
+		String [] args = {"-d", "java.awt.Desktop"};
+		DesignParser.main(args);
+		String content = new Scanner(new File("dot/out.dot")).useDelimiter("\\Z").next();
+		if (content.contains("<<Singleton>>")) {
+			// We're all good
+		} else {
+			fail();
+		}
+	}
+	
+	@SuppressWarnings("resource")
+	@Test
+	public void testSingletonCalendar() throws IOException {
+		String [] args = {"-d", "java.util.Calendar"};
+		DesignParser.main(args);
+		String content = new Scanner(new File("dot/out.dot")).useDelimiter("\\Z").next();
+		if (content.contains("<<Singleton>>")) {
+			// We're all good
+		} else {
+			fail();
+		}
+	}
+	
+	@SuppressWarnings("resource")
+	@Test
+	public void testSingletonStream() throws IOException {
+		String [] args = {"-d", "java.io.FilterInputStream"};
+		DesignParser.main(args);
+		String content = new Scanner(new File("dot/out.dot")).useDelimiter("\\Z").next();
+		if (content.contains("<<Singleton>>")) {
+			// We're all good
+		} else {
+			fail();
+		}
+	}
+	
 }
