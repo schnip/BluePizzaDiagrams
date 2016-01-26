b4ff3f11f3b1109d3231294efbd956455a9ef995
schnip <pingthegreenflag@gmail.com>
2016-01-25 22:35:14 -0500

    All the files have been made

---
+++ src/problem/asm/patternfinder/DecoratorFinder.java
@@ -0,0 +1,33 @@
+package problem.asm.patternfinder;
+
+import java.io.PrintWriter;
+
+import problem.asm.storage.MetaDataLibrary;
+
+public class DecoratorFinder implements IFindPatterns {
+
+	@Override
+	public void intake(MetaDataLibrary mdl) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void write(String className, PrintWriter writer) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void writeAttributes(String className, PrintWriter writer) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
