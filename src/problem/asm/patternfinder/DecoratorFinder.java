b5165f5210d9aeb88683a05cb961e7476eb6e488
NithinPerumal <peruman@rose-hulman.edu>
2016-01-27 00:06:00 -0500

    Filled in the blanks that schnips made like the first grader I am :)

--- src/problem/asm/patternfinder/DecoratorFinder.java
+++ src/problem/asm/patternfinder/DecoratorFinder.java
@@ -1,20 +1,82 @@
 package problem.asm.patternfinder;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import problem.asm.storage.ClassVolume;
+import problem.asm.storage.FieldPage;
 import problem.asm.storage.MetaDataLibrary;
+import problem.asm.storage.MethodBook;
 
 public class DecoratorFinder implements IFindPatterns {
-	
+
 	private Map<String, String> classToSpecial = new HashMap<String, String>();
 	private Map<String, String> edgeToLabel = new HashMap<String, String>();
 
 	@Override
 	public void intake(MetaDataLibrary mdl) {
-		// TODO Auto-generated method stub
+		for (ClassVolume cv : mdl.getClassVolume()) {
+			if (superInClassVolume(cv)) {
+				if (hasInterfaceAsField(cv) || hasSuperClassAsField(cv)) {
+					if (constructorTakesType(cv)) {
+						
+					}
+				}
+			}
+		}
 
+	}
+
+	public boolean superInClassVolume(ClassVolume c) {
+		if (!c.getSuperName().equals("java.lang.object")) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean constructorTakesType(ClassVolume cv) {
+		for (MethodBook mb : cv.getMethods()) {
+			if (mb.getName().equals("<init>")) {
+				List<String> li = mb.getArgTypes();
+				for (String arg : li) {
+					String comp = cv.getSuperName();
+					String[] intcomp = cv.getInterfaces();
+					if (arg.equals(comp) || arg.equals(intcomp)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	public boolean hasSuperClassAsField(ClassVolume cv) {
+		String comp = "";
+		if (cv.getSuperName() != "java.lang.object"){
+			comp = cv.getSuperName();
+		}
+		for (FieldPage fp : cv.getFields()) {
+			if (fp.getValue().toString().equals(comp)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean hasInterfaceAsField(ClassVolume cv) {
+		String[] check = cv.getInterfaces();
+		if (check.length == 0) return false;
+		for (String c : check) {
+			for (FieldPage fp : cv.getFields()) {
+				if (fp.getValue().toString().equals(c)) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	@Override
@@ -43,7 +105,7 @@ public class DecoratorFinder implements IFindPatterns {
 		if (edgeToLabel.keySet().contains(edgeDescription)) {
 			writer.print(", label = \"" + edgeToLabel.get(edgeDescription) + "\"");
 		}
-		
+
 	}
 
 }
