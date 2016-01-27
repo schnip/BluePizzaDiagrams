c7a40178875ceb6465f95c1f1d3b6bafebcbb03b
schnip <pingthegreenflag@gmail.com>
2016-01-26 23:20:16 -0500

    Same basic setup for decorator now has been done for adapter as well

--- src/problem/asm/patternfinder/AdapterFinder.java
+++ src/problem/asm/patternfinder/AdapterFinder.java
@@ -1,10 +1,15 @@
 package problem.asm.patternfinder;
 
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 import problem.asm.storage.MetaDataLibrary;
 
 public class AdapterFinder implements IFindPatterns {
+	
+	private Map<String, String> classToSpecial = new HashMap<String, String>();
+	private Map<String, String> edgeToLabel = new HashMap<String, String>();
 
 	@Override
 	public void intake(MetaDataLibrary mdl) {
@@ -14,25 +19,30 @@ public class AdapterFinder implements IFindPatterns {
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "adapter";
 	}
 
 	@Override
 	public void write(String className, PrintWriter writer) {
-		// TODO Auto-generated method stub
-		
+		if (classToSpecial.keySet().contains(className)) {
+			writer.println();
+			writer.print("\\<\\<" + classToSpecial.get(className) + "\\>\\>\\l");
+		}
 	}
 
 	@Override
 	public void writeAttributes(String className, PrintWriter writer) {
-		// TODO Auto-generated method stub
-		
+		if (classToSpecial.keySet().contains(className)) {
+			writer.print("fillcolor=red, style=filled,");
+			writer.println();
+		}
 	}
 
 	@Override
 	public void labelEdge(String edgeDescription, PrintWriter writer) {
-		// TODO Auto-generated method stub
+		if (edgeToLabel.keySet().contains(edgeDescription)) {
+			writer.print(", label = \"" + edgeToLabel.get(edgeDescription) + "\"");
+		}
 		
 	}
 
