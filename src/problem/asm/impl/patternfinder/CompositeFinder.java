5049a87d7f5d54fdb0f4fe80935f97a1ae697145
schnip <pingthegreenflag@gmail.com>
2016-02-03 23:26:54 -0500

    Primary coding sequence completed

--- src/problem/asm/impl/patternfinder/CompositeFinder.java
+++ src/problem/asm/impl/patternfinder/CompositeFinder.java
@@ -6,30 +6,78 @@ import java.util.Map;
 
 import problem.asm.api.patternfinder.IFindPatterns;
 import problem.asm.storage.ClassVolume;
+import problem.asm.storage.FieldPage;
 import problem.asm.storage.MetaDataLibrary;
+import problem.asm.storage.MethodBook;
+import problem.asm.storage.StU;
 
 public class CompositeFinder implements IFindPatterns {
 	
 	private Map<String, String> classToSpecial = new HashMap<String, String>();
+	private MetaDataLibrary mdl;
 
 	@Override
 	public void intake(MetaDataLibrary mdl) {
+		this.mdl = mdl;
 		// Find the components
 		for (ClassVolume cv : mdl.getClassVolume()) {
 			if (checkForTwoSelfMethods(cv) && checkForContainsSelfCollection(cv)) {
 				classToSpecial.putIfAbsent(cv.getName(), "component");
 			}
 		}
+		
+		// Find the composites
+		for (ClassVolume cv : mdl.getClassVolume()) {
+			for (String potentialComponent : classToSpecial.keySet()) {
+				if (isExtension(cv, potentialComponent))
+					if (hasTwoMethodsTaking(cv, potentialComponent))
+						if (hasCollectionOf(cv, potentialComponent))
+							classToSpecial.putIfAbsent(cv.getName(), "composite");
+			}
+		}
+		
+		// Find the leaves
+		for (ClassVolume cv : mdl.getClassVolume()) {
+			for (String potentialComponent : classToSpecial.keySet()) {
+				if (isExtension(cv, potentialComponent))
+					classToSpecial.putIfAbsent(cv.getName(), "leaf");
+			}
+		}
+	}
+
+	private boolean hasCollectionOf(ClassVolume cv, String potentialComponent) {
+		int count = 0;
+		for (FieldPage fp : cv.getFields()) {
+			if (fp.getSignature() != null)
+				if (StU.ehhEquals(StU.parseStringForT(fp.getSignature()), potentialComponent))
+					count++;
+		}
+		return count >= 1;
+	}
+
+	private boolean hasTwoMethodsTaking(ClassVolume cv, String potentialComponent) {
+		int count = 0;
+		for (MethodBook mb : cv.getMethods()) {
+			if (StU.ehhEquals(potentialComponent, mb.getArgTypes().get(0)) && mb.getArgTypes().size() == 1)
+				count++;
+		}
+		return count >= 2;
+	}
+
+	private boolean isExtension(ClassVolume cv, String potentialComponent) {
+		if (StU.ehhEquals(cv.getSuperName(), potentialComponent))
+			return true;
+		if (mdl.contains(cv.getSuperName()))
+			return isExtension(mdl.getClassByString(cv.getSuperName()), potentialComponent);
+		return false;
 	}
 
 	private boolean checkForContainsSelfCollection(ClassVolume cv) {
-		// TODO Auto-generated method stub
-		return false;
+		return hasCollectionOf(cv, cv.getName());
 	}
 
 	private boolean checkForTwoSelfMethods(ClassVolume cv) {
-		// TODO Auto-generated method stub
-		return false;
+		return hasTwoMethodsTaking(cv, cv.getName());
 	}
 
 	@Override
