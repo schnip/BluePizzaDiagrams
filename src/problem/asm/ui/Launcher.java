165101b0e67d33e1641ee8d5a819ef2edf8cc3de
schnip <pingthegreenflag@gmail.com>
2016-02-15 23:03:46 -0500

    DesignParser has the new CLI funcitonality patched in

---
+++ src/problem/asm/ui/Launcher.java
@@ -0,0 +1,15 @@
+package problem.asm.ui;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		LandingPage lp = new LandingPage();
+		parseAndDo(lp.getConfigPath());
+	}
+
+	public static void parseAndDo(String configPath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
