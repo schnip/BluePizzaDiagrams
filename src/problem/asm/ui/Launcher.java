f1fb61eb97b15a901224219ddb4db58c5a139f00
schnip <pingthegreenflag@gmail.com>
2016-02-15 23:27:46 -0500

    Added a sample json

--- src/problem/asm/ui/Launcher.java
+++ src/problem/asm/ui/Launcher.java
@@ -1,6 +1,12 @@
 package problem.asm.ui;
 
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
+import problem.asm.storage.StaticLibraryHolder;
 
 public class Launcher {
 
@@ -10,8 +16,20 @@ public class Launcher {
 	}
 
 	public static void parseAndDo(String configPath) {
+		StaticLibraryHolder.initializeLibrary();
 		JSONParser parser = new JSONParser();
-		
+		try {
+			JSONObject obj = (JSONObject) parser.parse(new FileReader(configPath));
+			JSONArray jarry = (JSONArray) obj.get("classes");
+			for (Object o : jarry) {
+				JSONObject jo = (JSONObject) o;
+				StaticLibraryHolder.parseClass(jo.toString());
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
