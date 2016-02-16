e3caa8540d8433299a904099f8ee3dec6acdabf2
schnip <pingthegreenflag@gmail.com>
2016-02-15 23:45:30 -0500

    More to the json

--- src/problem/asm/ui/Launcher.java
+++ src/problem/asm/ui/Launcher.java
@@ -9,6 +9,11 @@ import org.json.simple.parser.JSONParser;
 import problem.asm.storage.StaticLibraryHolder;
 
 public class Launcher {
+	
+	private static String ouputDir;
+	private static String dotPath;
+	private static String diagramType;
+	
 
 	public static void main(String[] args) {
 		LandingPage lp = new LandingPage();
@@ -22,9 +27,9 @@ public class Launcher {
 			JSONObject obj = (JSONObject) parser.parse(new FileReader(configPath));
 			JSONArray jarry = (JSONArray) obj.get("classes");
 			for (Object o : jarry) {
-				JSONObject jo = (JSONObject) o;
-				StaticLibraryHolder.parseClass(jo.toString());
+				StaticLibraryHolder.parseClass((String) o);
 			}
+			diagramType = (String) obj.get("type");
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
