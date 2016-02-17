ab9f272bc281368264e9f7576edf426377baeec3
schnip <pingthegreenflag@gmail.com>
2016-02-17 01:18:20 -0500

    ugh

--- src/problem/asm/ui/ResultMaker.java
+++ src/problem/asm/ui/ResultMaker.java
@@ -1,5 +1,6 @@
 package problem.asm.ui;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import problem.asm.outputer.DiagramOutputer;
@@ -26,7 +27,28 @@ public class ResultMaker implements IMakeResults {
 			IOutputData iod = new DiagramOutputer(path, phases);
 			iod.outputData(mdl);
 		} else {
+			Set<String> newPhases = new HashSet<String>();
+			Set<String> newClasses = new HashSet<String>();
+			// Load the two sets
+			for (PatternCollection pattern : patC.getSubCollection()) {
+				if (pattern.isChecked()) {
+					newPhases.add(pattern.getTitle());
+				}
+				for (PatternCollection instance : pattern.getSubCollection()) {
+					if (instance.isChecked()) {
+						newClasses.add(instance.getTitle());
+						// TODO FINISH THIS STUFF
+					}
+				}
+			}
 			
+			// Now that they have been loaded, make the new mdl and run the outputter on it
+			MetaDataLibrary newMdl = new MetaDataLibrary();
+			for (String s : newClasses) {
+				newMdl.addClass(mdl.getClassByString(s));
+			}
+			IOutputData iod = new DiagramOutputer(path, newPhases);
+			iod.outputData(newMdl);
 		}
 		return runDot();
 	}
