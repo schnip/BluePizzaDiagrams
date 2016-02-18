60c2447a673cc16a83198b576eea157cceb30f85
schnip <pingthegreenflag@gmail.com>
2016-02-17 21:49:11 -0500

    The result maker should be ready to go

--- src/problem/asm/ui/ResultMaker.java
+++ src/problem/asm/ui/ResultMaker.java
@@ -3,12 +3,15 @@ package problem.asm.ui;
 import java.util.HashSet;
 import java.util.Set;
 
+import problem.asm.api.patternfinder.IFindPatterns;
+import problem.asm.api.patternfinder.IPatternInstance;
 import problem.asm.outputer.DiagramOutputer;
 import problem.asm.outputer.IOutputData;
 import problem.asm.storage.MetaDataLibrary;
+import problem.asm.storage.StU;
 
 public class ResultMaker implements IMakeResults {
-	
+
 	private MetaDataLibrary mdl;
 	private String dotPath;
 	private String outputDir;
@@ -31,18 +34,30 @@ public class ResultMaker implements IMakeResults {
 			Set<String> newClasses = new HashSet<String>();
 			// Load the two sets
 			for (PatternCollection pattern : patC.getSubCollection()) {
+				boolean all = false;
 				if (pattern.isChecked()) {
 					newPhases.add(pattern.getTitle());
+					all = true;
 				}
 				for (PatternCollection instance : pattern.getSubCollection()) {
-					if (instance.isChecked()) {
-						newClasses.add(instance.getTitle());
-						// TODO FINISH THIS STUFF
+					boolean allinst = false;
+					if (instance.isChecked() || all) {
+						allinst = true;
+					}
+					IFindPatterns ifp = loadAndGetPatternFinderByName(pattern.getTitle());
+					for (IPatternInstance ipi : ifp.getInstances()) {
+						if (ipi.getTitle().equals(instance.getTitle()) || allinst || all) {
+							newPhases.add(instance.getTitle());
+							for (String inst : ipi.getParticipantClasses()) {
+								newClasses.add(inst);
+							}
+						}
 					}
 				}
 			}
-			
-			// Now that they have been loaded, make the new mdl and run the outputter on it
+
+			// Now that they have been loaded, make the new mdl and run the
+			// outputter on it
 			MetaDataLibrary newMdl = new MetaDataLibrary();
 			for (String s : newClasses) {
 				newMdl.addClass(mdl.getClassByString(s));
@@ -52,7 +67,19 @@ public class ResultMaker implements IMakeResults {
 		}
 		return runDot();
 	}
-	
+
+	private IFindPatterns loadAndGetPatternFinderByName(String title) {
+		Set<IFindPatterns> patternfinders = new HashSet<IFindPatterns>();
+		StU.loadClasses("bin/problem/asm/impl/patternfinder", patternfinders );
+		for (IFindPatterns ifp : patternfinders) {
+			if (ifp.getName().equals(title)) {
+				ifp.intake(mdl);
+				return ifp;
+			}
+		}
+		return null;
+	}
+
 	private String runDot() {
 		// Assume that this is being run on linux
 		Process p;
@@ -60,10 +87,9 @@ public class ResultMaker implements IMakeResults {
 			p = Runtime.getRuntime().exec(dotPath + " -O -Tpng " + path);
 			p.waitFor();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return outputDir + "out.dot.png";
 	}
 
