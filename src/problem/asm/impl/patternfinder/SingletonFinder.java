9b40e8093a86a03e3d0b74bba870510132019643
schnip <pingthegreenflag@gmail.com>
2016-02-17 23:42:28 -0500

    reverted

--- src/problem/asm/impl/patternfinder/SingletonFinder.java
+++ src/problem/asm/impl/patternfinder/SingletonFinder.java
@@ -109,8 +109,6 @@ public class SingletonFinder implements IFindPatterns {
 
 	@Override
 	public Iterable<IPatternInstance> getInstances() {
-		System.out.println("nah, im cool");
-		System.out.println("I have " + patInst.size() + " instances");
 		return patInst;
 	}
 
